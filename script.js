406d5eccad986053d9e45d1490ced6c0cc2e3dce
Nadeem Irshad Pirzada <pirzadanadeem5@gmail.com>
2025-08-21 15:56:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// Add current year in footer
+document.getElementById("year").textContent = new Date().getFullYear();
 
+// Toggle nav menu for small screens
+const toggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+toggle.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+});
